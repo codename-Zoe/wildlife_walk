b911c11360327f83c11c92963ad598da440b8d07
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2022-08-15 15:02:08 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function showObservations(observations, title) {
 function showAll() {
   // Get all the observations from our data.js and format them so we can work with the data
   const observations = getAllObservations();
-  showObservations(observations, 'All Species');
+  showObservations(observations, "All Species");
 }
 
 // Show native species
@@ -43,7 +43,7 @@ function showOnlyNative() {
   // Filter out any that aren't native species
   const native = filterOnlyNative(observations);
 
-  showObservations(native, 'Only Native Species');
+  showObservations(native, "Only Native Species");
 }
 
 // Show introduced species
@@ -53,7 +53,7 @@ function showOnlyIntroduced() {
   // Filter out any that aren't introduced species
   const introduced = filterOnlyIntroduced(observations);
 
-  showObservations(introduced, 'Only Introduced Species');
+  showObservations(introduced, "Only Introduced Species");
 }
 
 function start() {
@@ -73,8 +73,8 @@ function start() {
   btnOnlyIntroduced.onclick = () => showOnlyIntroduced();
 
   // Connect the search form
-  const searchForm = document.querySelector('form');
-  searchForm.onsubmit = function(event) {
+  const searchForm = document.querySelector("form");
+  searchForm.onsubmit = function (event) {
     // Stop the form from trying to submit over the network. We'll process the data here.
     event.preventDefault();
 
@@ -82,12 +82,11 @@ function start() {
     // search() function (already defined in js/search.js)
     let value = document.getElementById("search-box").value;
     search(value);
-    console.log('TODO - call search() function with value from search form');
-
+    console.log("TODO - call search() function with value from search form");
   };
 
   // Show all observations at first
   showAll();
-};
+}
 
 window.onload = start;
--- js/validate.js
+++ js/validate.js
@@ -5,57 +5,65 @@ function validate(event) {
   // invalid, show the appropriate error message in the form, and stop the
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
-  let lat = document.querySelector('#lat');
-  let long = document.querySelector('#long');
+
+  let lat = document.querySelector("#lat");
+  let long = document.querySelector("#long");
   let flag = false;
   lat = parseFloat(lat.value);
   long = parseFloat(long.value);
 
-  if((lat > 90 || lat < -90 || isNaN(lat)) && (long > 180 || long < -180 || isNaN(long))){
-    document.querySelector('#lat-warning').innerText = "* must be a valid Latitude (-90 to 90)";
+  if (
+    (lat > 90 || lat < -90 || isNaN(lat)) &&
+    (long > 180 || long < -180 || isNaN(long))
+  ) {
+    document.querySelector("#lat-warning").innerText =
+      "* must be a valid Latitude (-90 to 90)";
     event.preventDefault();
-  }
-  else{
-    if(lat > 90 || lat < -90 || isNaN(lat)){
-      document.querySelector('#lat-warning').innerText = "* must be a valid Latitude (-90 to 90)";
+  } else {
+    if (lat > 90 || lat < -90 || isNaN(lat)) {
+      document.querySelector("#lat-warning").innerText =
+        "* must be a valid Latitude (-90 to 90)";
       event.preventDefault();
+    } else {
+      document.querySelector("#lat-warning").innerText = "*";
     }
-    else{
-      document.querySelector('#lat-warning').innerText = "*";
-    }
-    if(long > 180 || long < -180 || isNaN(long)){
-      document.querySelector('#long-warning').innerText = "* must be a valid Longitude (-180 to 180)";
+    if (long > 180 || long < -180 || isNaN(long)) {
+      document.querySelector("#long-warning").innerText =
+        "* must be a valid Longitude (-180 to 180)";
       event.preventDefault();
+    } else {
+      document.querySelector("#long-warning").innerText = "*";
+      flag = true;
     }
-    else{
-      document.querySelector('#long-warning').innerText = "*";
-      }
   }
-  console.log('TODO - validate the longitude, latitude values before submitting');
-  return flag;
 
-  // if(long > 180 || long < -180 || isNaN(long)){
-  //   document.querySelector('#long-warning').innerText = "* must be a valid Longitude (-180 to 180)";
-  //   flag = false;
+  // display the error message at once
+
+  // if (lat > 90 || lat < -90 || isNaN(lat)) {
+  //   document.querySelector("#lat-warning").innerText =
+  //     "* must be a valid Latitude (-90 to 90)";
   //   event.preventDefault();
-  // }
-  // else{
-  //   document.querySelector('#long-warning').innerText = "*";
-  //   flag = true;
-  // }
-  // else if (lat < 90 || lat > -90 || !isNaN(lat)){
-  //   document.querySelector('#lat-warning').innerText = "*";
-  //   flag = true;)
-  // }
-  // else if(long < 180 || long > -180 || isNaN(long)){
-  //   document.querySelector('#long-warning').innerText = "*";
+  // } else {
+  //   document.querySelector("#lat-warning").innerText = "*";
   //   flag = true;
   // }
 
+  // if (long > 180 || long < -180 || isNaN(long)) {
+  //   document.querySelector("#long-warning").innerText =
+  //     "* must be a valid Longitude (-180 to 180)";
+  //   event.preventDefault();
+  // } else {
+  //   document.querySelector("#long-warning").innerText = "*";
+  //   flag = true;
+  // }
+  console.log(
+    "TODO - validate the longitude, latitude values before submitting"
+  );
+  return flag;
 }
 
 // Wait for the window to load, then set up the submit event handler for the form.
-window.onload = function() {
-  const form = document.querySelector('form');
+window.onload = function () {
+  const form = document.querySelector("form");
   form.onsubmit = validate;
 };
